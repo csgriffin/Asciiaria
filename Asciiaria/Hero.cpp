dd28742e14b0d6718b2aeb8ade3aba275cc1abc6
Thomas Lourenco <TSLourenco@WPI.edu>
2016-10-06 21:43:38 -0400

    Commiting changes.

--- Asciiaria/Hero.cpp
+++ Asciiaria/Hero.cpp
@@ -4,15 +4,19 @@
 #include "LogManager.h"
 #include "EventKeyboard.h"
 #include "EventOut.h"
-
-
+#include "EventMouse.h"
 
 Hero::Hero()
 {
 	Character();
+
+	// Creates a reticle.
+	p_reticle = new Reticle();
+	p_reticle->draw();
 }
 
 int Hero::eventHandler(const df::Event * p_e) {
+
 	if (p_e->getType() == df::KEYBOARD_EVENT) {
 		const df::EventKeyboard *p_s = dynamic_cast <const df::EventKeyboard *> (p_e);
 		if (p_s->getKeyboardAction() == df::KEY_DOWN) {
